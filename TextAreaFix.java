335c32396e52964d51270a774be2981f35f30ee1
Miki Marshall <47702942+Mikibits@users.noreply.github.com>
2019-02-16 18:50:38 -0800

    Add files via upload

---
+++ TextAreaFix.java
@@ -0,0 +1,45 @@
+/*
+ * Miki's Personal Utility Classes for Java - Reusable classes, Java class
+ *         wrappers to add/fix features, and other handy bits I can reuse.
+ *
+ *         Created by:  Miki R. Marshall
+ *         Tested with: Java 9, 10
+ */
+
+package mikilib;
+
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Thin extension of the JavaFX TextArea class, adding an event filter and
+ * handle that changes the default TAB behavior to be more like other fields,
+ * where:
+ *  - TAB moves focus to the next focusable control, and
+ *  - SHIFT-TAB moves to the previous.
+ */
+public class TextAreaFix extends TextArea {
+    public TextAreaFix() {
+        addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            KeyCode code = event.getCode();
+            Node node = (Node) event.getSource();
+
+            // Fix Tab event
+            if ((code == KeyCode.TAB) && !event.isShiftDown() &&
+                    !event.isControlDown()) {
+                // Consume original event and make our own
+                event.consume();
+
+                // Re-fire event with Control key down status reversed
+                node.fireEvent(new KeyEvent(event.getSource(),
+                            event.getTarget(), event.getEventType(),
+                            event.getCharacter(), event.getText(),
+                            event.getCode(), event.isShiftDown(),
+                            true,
+                            event.isAltDown(), event.isMetaDown()));
+            }
+        });
+    }
+}
